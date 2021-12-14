cecc762dff414b14d34ad31d6c920b4fe13bad98
unknown <john92cho@gmail.com>
2021-12-13 21:52:51 -0500

    working case of transfer with commands

--- src/TestSocketClient.java
+++ src/TestSocketClient.java
@@ -2,25 +2,52 @@
 import java.net.*;
 import java.io.*;
 public class TestSocketClient {
+	 static Socket socket = null;
+     static String host = "127.0.0.1";
     public static void main(String[] args) throws IOException {
-        Socket socket = null;
-        String host = "127.0.0.1";
+       
 
         socket = new Socket(host, 4444);
-        
-        File file = new File("test.txt");
-        // Get the size of the file
-        byte[] bytes = new byte[8192];
-        InputStream in = new FileInputStream(file);
-        OutputStream out = socket.getOutputStream();
-        
-        int count;
-        while ((count = in.read(bytes)) > 0) {
-            out.write(bytes, 0, count);
+        //sendFile();
+        System.out.println("client");
+        try( 
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ){
+        	Boolean end = false;
+        	
+            while(!end) {
+            	String responseFromServer = in.readLine();
+            	if(responseFromServer.equals("ready")) {
+            		out.println(stdIn.readLine());
+            		sendFile();
+            	}
+            	System.out.println(responseFromServer);
+            	out.println(stdIn.readLine());
+            }
+            System.out.println("ending");
+            socket.close();
+        }catch(IOException e) {
+        	System.out.println(e);
         }
-
-        out.close();
-        in.close();
-        socket.close();
+    }
+        
+        
+    
+    public static void sendFile() throws IOException {
+    	Socket fileSocket = new Socket(host, 4445);
+	   File file = new File("test.txt");
+	   // Get the size of the file
+	   byte[] bytes = new byte[8192];
+	   InputStream in = new FileInputStream(file);
+	   OutputStream out = fileSocket.getOutputStream();
+	   
+	   int count;
+	   while ((count = in.read(bytes)) > 0) {
+	       out.write(bytes, 0, count);
+	   }
+	   out.close();
+	   in.close();
     }
 }
--- src/TestSocketServer.java
+++ src/TestSocketServer.java
@@ -1,7 +1,10 @@
 import java.net.*;
 import java.io.*;
 public class TestSocketServer {
-
+	static Socket socket = null;
+    static InputStream in = null;
+    static OutputStream out = null;
+     
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
 
@@ -10,39 +13,88 @@ public class TestSocketServer {
         } catch (IOException ex) {
             System.out.println("Can't setup server on this port number. ");
         }
-
-        Socket socket = null;
-        InputStream in = null;
-        OutputStream out = null;
-        
+       
         try {
             socket = serverSocket.accept();
+            
         } catch (IOException ex) {
             System.out.println("Can't accept client connection. ");
         }
         
-        try {
-            in = socket.getInputStream();
-        } catch (IOException ex) {
-            System.out.println("Can't get socket input stream. ");
-        }
-
-        try {
-            out = new FileOutputStream("test2.txt");
-        } catch (FileNotFoundException ex) {
-            System.out.println("File not found. ");
-        }
-
-        byte[] bytes = new byte[8192];
-
-        int count;
-        while ((count = in.read(bytes)) > 0) {
-            out.write(bytes, 0, count);
-        }
-
-        out.close();
-        in.close();
-        socket.close();
+        handleClientCommands();
         serverSocket.close();
+    }
+    
+    public static void handleClientCommands() {
+	  try(
+			PrintWriter out =new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			  )  { 
+		 String inputLine;
+		 out.println("you have connected to the server");
+		  while ((inputLine = in.readLine()) != null) {
+			  switch(inputLine) {
+			  case "upload":
+				  createFile("text2.txt", out, inputLine);
+				  break;
+			  default:
+				  out.println("server response: " + inputLine);
+				  break;
+			  }
+			  if(inputLine.equals("end")) {
+	        	  out.println("server response: ending your session");
+	        	  break;
+	          }
+	         
+
+		  }
+	 }catch(IOException e) {
+		 System.out.println(e);
+		 System.out.println("no fucking clue why it is failing here");
+	 }
+    }
+    
+    public static void createFile(String name, PrintWriter output, String input) throws IOException {
+    	Socket fileSocket = null; 
+    	ServerSocket fileServerSocket = null;
+		 InputStream in = null;
+		 OutputStream out = null;
+    	 
+    	 try {
+    		 fileServerSocket = new ServerSocket(4445);
+    	 } catch(IOException e) {
+    		 System.out.println(e);
+    	 }
+    	 
+    	 try {
+    		 output.println("ready");
+    		 fileSocket = fileServerSocket.accept();   
+    		 
+         } catch (IOException ex) {
+             System.out.println("Can't accept client connection. ");
+         }
+    	 
+    		 try {
+                 in =  fileSocket.getInputStream();
+             } catch (IOException ex) {
+                 System.out.println("Can't get socket input stream. ");
+             }
+
+             try {
+                 out = new FileOutputStream("test2.txt");
+             } catch (FileNotFoundException ex) {
+                 System.out.println("File not found. ");
+             }
+
+             byte[] bytes = new byte[8192];
+
+             int count;
+             while ((count = in.read(bytes)) > 0) {
+                 out.write(bytes, 0, count);
+             }
+	         out.close();
+	         in.close();
+    	
+    	
     }
 }
